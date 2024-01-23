189aeeda904a1daf2da23b55067c1436cf2cc039
Ysmailov Nursultan <nursysmailovv@gmail.com>
2024-01-23 19:44:17 +0600

    first homework

--- bin/firstproject.dart
+++ bin/firstproject.dart
@@ -1,36 +1,38 @@
 void main() {
-  // String greeting = "myare";
-  // String myName = "$greeting aktan\nabai";
-  // String result = "${greeting + myName} lal";
+  //Вывод типа
+  String greeting = "Hello,Dart!";
+  print(greeting.runtimeType);
 
-  // String result = ''' asd
-  //                   asd
-  // asd''';
+  //Содержит ли подстроку
+  String check = "Dart programming language";
+  print(check.contains('dart'));
 
-  // int myNumber = 1;
+  //Вывод в верхнем регистре
+  String upperCase = "dart is fun";
+  print(upperCase.toUpperCase());
 
-  // print(myNumber % 2);
+  //Вывод в нижнем регистре
+  String lowerCase = "DART IS COOL";
+  print(lowerCase.toLowerCase());
 
-  // print(greeting.replaceAll('from', 'name'));
-
-  String name = "10asd";
-  // int number = 0;
-  // number = 1;
-
-  // print(number.toString().runtimeType);
-  // print(number + int.parse(name));
-  // print(int.tryParse(name));
-
-  int result = int.tryParse(name) ?? 0;
-
-  int numbur = int.tryParse('12') ?? 0;
-
+  //Объеденение строк
   String first = 'Hello';
   String myName = 'Dart';
   print('$first $myName');
 
-  String spaces = 'Replace spaces with underscores';
-  print(spaces.replaceAll(' ','_'));
+  //Проверка на пустоту
+  String spaces = '            ';
+  print(spaces.isEmpty);
 
-  print(numbur.runtimeType);
+  //Проверка длины
+  String checkLength = 'Dart Programming';
+  print(checkLength.length);
+
+  //Удаление пробелов
+  String trimming = 'Trim me!';
+  print(trimming.trim());
+
+  //Замена символов
+  String replacing = 'Replace spaces with underscores';
+  print(replacing.replaceAll(' ', '_'));
 }
